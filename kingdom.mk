50ef0bbbc03128f17dde4f166027d2873fb46546
Jan Forman <info@janforman.com>
2019-04-05 07:34:09 +0000

    Use lineage custom legacy camera provider
    Change-Id: I60a838f9bbcb4b30ed36b48e1ca21d4bfdfac4aa

--- kingdom.mk
+++ kingdom.mk
@@ -112,8 +112,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.msm8974 \
     Snap
 
