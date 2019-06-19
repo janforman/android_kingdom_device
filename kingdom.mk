3ee88db320aa993d702a0e80d2f6b94d4fc8b023
Jan Forman <info@janforman.com>
2019-06-19 08:18:52 +0000

    Kernel fixes
    Change-Id: I120824fd4dc1442d15681f2fce0c232a69bb724d

--- kingdom.mk
+++ kingdom.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2019-03-29
+    ro.lineage.build.vendor_security_patch=2019-06-19
 
 # Vibrator
 PRODUCT_PACKAGES += \
