3affce81cb2b208121ddd923752848d2fb7f0b69
Jan Forman <info@janforman.com>
2020-04-16 13:42:29 +0000

    kingdom: Remove more deprecated display flags
    Change-Id: I4c4c1292c08ffc37639c0550f40e4707916c8165

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,16 +175,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
